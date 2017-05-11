f0bba889788a8d213f5a56af429aa7add20370b3
dxbluff <yusufjonov98@gmail.com>
2017-05-11 22:48:47 +0300

    first git

---
+++ HuffMan/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+struct Node
+{
+	int a;
+	char c;
+	Node *left, *right;
+};
+
+int main()
+{
+	string a("Hello it's test text");
+	map<char, int> myMap;
+	
+	for (int i = 0; i < a.length(); i++)
+	{
+		myMap[a[i]]++;
+	}
+
+
+	map<char, int>::iterator it;
+
+	for (it = myMap.begin(); it != myMap.end(); ++it)
+	{
+		cout << "[" << it->first << "]->" << it->second << endl;
+	}
+
+	cout << endl;
+	return 0;
+}
