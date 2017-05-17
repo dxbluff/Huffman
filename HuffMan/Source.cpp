246fa8c974914bf7a714b167224595071d9ea51f
Yusufjonov Usufjon <yusufjonov98@gmail.com>
2017-05-18 02:59:12 +0300

    Add files via upload

--- HuffMan/Source.cpp
+++ HuffMan/Source.cpp
@@ -1,36 +1,24 @@
+#include "Huffman.h"
 #include <iostream>
-#include <map>
-#include <string>
 
 using namespace std;
 
-struct Node
-{
-	int a;
-	char c;
-	Node *left, *right;
-};
 
 int main()
 {
-	string a("Hello it's test text");
-	map<char, int> myMap;
-	myMap['c'] = 5;
-	cout << myMap['c'];
-	
-	for (int i = 0; i < a.length(); i++)
-	{
-		myMap[a[i]]++;
-	}
+	//Huffman a(0, /*"Helloooooo beep"*/ "itismystriiiiing!!!!");
+	Huffman b(1, "1.txt");
+
+	std::cout << std::endl;
+	b.print_codes();
+	std::cout << std::endl;
+	b.print_tree();
+	std::cout << std::endl;
+
+//	b.print_encoded();
+	std::cout << std::endl;
+	std::cout << std::endl;
 
 
-	map<char, int>::iterator it;
-
-	for (it = myMap.begin(); it != myMap.end(); ++it)
-	{
-		cout << "[" << it->first << "]->" << it->second << endl;
-	}
-
-	cout << endl;
-	return 0;
+	b.encode();
 }
